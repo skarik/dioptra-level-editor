d104b0710d2cec336db330b84467127b078e4e8d
skarik <josh@epichousestudios.com>
2026-04-15 09:27:28 -0700

    Add WASD input to the player test pulled from the old proto

--- Test5-FPSMap/test-scripts/player-test-fps.gd
+++ Test5-FPSMap/test-scripts/player-test-fps.gd
@@ -82,7 +82,7 @@ var mStabilizerRotateCamera := Vector4(0, 0, 0, 0); 	## Stabilizer for camera ro
 
 #------------------------------------------------------------------------------#
 
-func _ready():
+func _ready() -> void:
 	_camera = $Camera3D as Camera3D;
 	_model = $CharacterModel as PlayerTest8ModelObject;
 	assert(_camera != null);
@@ -95,7 +95,7 @@ func _ready():
 	_model_skeleton.add_child(_head_ik);
 	#_head_ik.owner = _model_skeleton;
 	_animation_player = _model.get_node("AnimationPlayer") as AnimationPlayer;
-	_animation_player.mixer_applied.connect(_update_camera); 
+	#_animation_player.mixer_applied.connect(_update_camera); 
 	
 	_model_bone_eye_node = BoneAttachment3D.new();
 	_model_bone_eye_node.bone_idx = _model_bone_eye;
@@ -103,6 +103,9 @@ func _ready():
 	_model_skeleton.add_child(_model_bone_eye_node);
 	pass
 	
+func clampInputs() -> void:
+	mCameraForwardAngle.x = clamp(mCameraForwardAngle.x, deg_to_rad(-100.0), deg_to_rad(100.0));
+
 func _unhandled_input(event: InputEvent) -> void:
 	if (event is InputEventMouseMotion):
 		const rotationSpeed = PI / 180.0 * 0.5; #todo: make 0.5 sensitivity value
@@ -122,11 +125,55 @@ func _unhandled_input(event: InputEvent) -> void:
 			mJumpState.updateValue(false);
 	return
 
+func _process(delta: float) -> void:
+	_process_animation(delta);
+	_process_camera();
+	_process_stabilizer(delta);
+	pass
+	
+func _physics_process(delta: float) -> void:
+	_physics_process_input_motion(delta);
+	_physics_process_motion_collision(delta);
+		
+	pass
+
+#------------------------------------------------------------------------------#
+
 func queue_remote_free(node : Node) -> void:
 	node.queue_free();
-
-func _process(delta: float) -> void:
-	# Animations
+	
+#------------------------------------------------------------------------------#
+	
+func _process_camera() -> void:
+	# Update the forward direction
+	mCameraForwardDirection = Vector2.UP.rotated(-mCameraForwardAngle.y);
+		
+	var forward := Vector3.FORWARD \
+		.rotated(Vector3.RIGHT, mCameraForwardAngle.x) \
+		.rotated(Vector3.UP, mCameraForwardAngle.y);
+		
+	# Update where the camera is:
+	#var eyeTransform := _model_skeleton.get_bone_global_pose(_model_bone_eye);
+	#var cameraCenterPosition := _model.transform * eyeTransform * Vector3.ZERO + mStablizerOffsetCamera;
+	var cameraCenterPosition := _model.transform * _model_bone_eye_node.transform * Vector3.ZERO + mStablizerOffsetCamera;
+	_camera.position = forward * 0.05 + cameraCenterPosition;
+	_camera.rotation = Vector3(mCameraForwardAngle.x, mCameraForwardAngle.y, 0);
+	
+	# Update rotating the head ik
+	_head_ik.camera_angle = mCameraForwardAngle;
+	
+func _process_stabilizer(delta: float) -> void:
+	# Smooth out offsets:
+	var smoothWeight0 := 1.0 - exp(-8.0 * delta);
+	var smoothWeight1 := 1.0 - exp(-16.0 * delta);
+	var smoothWeight2 := 1.0 - exp(-40.0 * delta);
+	mStablizerOffsetCamera = mStablizerOffsetCamera.lerp(Vector3.ZERO, smoothWeight0);
+	mStablizerOffsetModel = mStablizerOffsetModel.lerp(Vector3.ZERO, smoothWeight1);
+	mStabilizerRotateCamera = mStabilizerRotateCamera.lerp(Vector4.ZERO, smoothWeight2);
+	
+	
+func _process_animation(delta: float) -> void:
+		# Animations
 	_animation_player.play("anim test 2/Idle 1");
 	
 	# Update animator
@@ -137,26 +184,146 @@ func _process(delta: float) -> void:
 		#animationPlayer.advance(Framerate);
 		#mModelUpdateTimer -= Framerate;
 	_animation_player.advance(delta);
-	
-func _update_camera() -> void:
-	# Update the forward direction
-	mCameraForwardDirection = Vector2.UP.rotated(-mCameraForwardAngle.y);
-		
-	var forward := Vector3.FORWARD \
-		.rotated(Vector3.RIGHT, mCameraForwardAngle.x) \
-		.rotated(Vector3.UP, mCameraForwardAngle.y);
-		
-	# Update where the camera is:
-	var eyeTransform := _model_skeleton.get_bone_global_pose(_model_bone_eye);
-	#var cameraCenterPosition := _model.transform * eyeTransform * Vector3.ZERO + mStablizerOffsetCamera;
-	var cameraCenterPosition := _model.transform * _model_bone_eye_node.transform * Vector3.ZERO + mStablizerOffsetCamera;
-	_camera.position = forward * 0.05 + cameraCenterPosition;
-	_camera.rotation = Vector3(mCameraForwardAngle.x, mCameraForwardAngle.y, 0);
-	
-	# Update rotating the head ik
-	_head_ik.camera_angle = mCameraForwardAngle;
+	pass
 	
 #------------------------------------------------------------------------------#
 
-func clampInputs() -> void:
-	mCameraForwardAngle.x = clamp(mCameraForwardAngle.x, deg_to_rad(-100.0), deg_to_rad(100.0));
+func _physics_process_input_motion(delta : float) -> void:
+	# Get motion input impulse from the input & camera angle
+	mInputVector = Vector2(
+		Input.get_axis("move_left", "move_right"),
+		-Input.get_axis("move_back", "move_forward")).normalized();
+	var rotatedInputVector : Vector2 = mInputVector.rotated(-mCameraForwardAngle.y);
+	
+	# Extract flat motion from velocity
+	mFullMotion = self.velocity;
+	mFlatMotion = Vector2(mFullMotion.x, mFullMotion.z);
+	
+	# Apply next motion
+	var possibleNextMotion : Vector2 = mFlatMotion;
+	var possibleMaxSpeed : float = cMaxMoveSpeed;
+	if (mSprintState.is_enabled()):
+		possibleMaxSpeed = cMaxSprintSpeed;
+	if (mDisableSpeedLimitUntilGround):
+		possibleMaxSpeed = max(possibleMaxSpeed, cGrindSpeed);
+		
+	if (not mDashing):
+		# Calculate both motion accel and friction accel
+		var targetDelta : Vector2 = (rotatedInputVector * possibleMaxSpeed) - mFlatMotion;
+		var targetDeltaLength : float = targetDelta.length();
+		var targetDeltaNormalized : Vector2 = targetDelta / max(0.0001, targetDeltaLength);
+		var possibleNextMotionAccel : Vector2 = mFlatMotion + targetDeltaNormalized * min(targetDeltaLength, cGroundAcceleration * delta);
+		var possibleNextMotionFrict : Vector2 = mFlatMotion + targetDeltaNormalized * min(targetDeltaLength, cGroundFriction * delta);
+		
+		# Apply friction if the friction is closer to stopping, or the friction is in a different direction than the acceleration
+		possibleNextMotion = possibleNextMotionAccel;
+		var isBrakingFaster : bool     = possibleNextMotionFrict.length_squared() < possibleNextMotionAccel.length_squared();
+		var isChangingDirection : bool = possibleNextMotionFrict.dot(possibleNextMotionAccel) < 0.0;
+		
+		if (isBrakingFaster or isChangingDirection):
+			possibleNextMotion = possibleNextMotionFrict;
+			#if isChangingDirection:
+				#var juiceNode : Node3D = mAssetJuice.instantiate()
+				#juiceNode.position = position
+				#juiceNode.rotation = Vector3(0, mFlatMotion.angle_to(Vector2.RIGHT), 0);
+				#get_parent().add_child(juiceNode) # probably bad
+			
+	elif (mDashing):
+		# We dash:
+		possibleNextMotion = mDashingDirection * lerp(cDashSpeed, (cMaxMoveSpeed + cDashSpeed) * 0.5, mDashingTime / cDashTime);
+		# State tracking for the dash:
+		mDashingTime += delta;
+		if (mDashingTime > cDashTime):
+			mDashing = false;
+			
+	# Use the chosen motion!
+	mFlatMotion = possibleNextMotion;
+	
+func _physics_process_motion_collision(delta : float) -> void:
+	# Collision Steps:
+	# 1. Do stair checks in the given "flat" motion
+	#    1b. Generate Z offset
+	# 2. Recombine w/ motion and move & collide normal
+	
+	var lHasGround : bool = false;
+	var lPositionStart : Vector3 = self.position;
+	
+	# Stair check
+	# Set up "flat" motion:
+	mFullMotion = Vector3(mFlatMotion.x, 0, mFlatMotion.y);
+	# Move with contact:
+	self.position = lPositionStart;
+	var lCollisionStair0 :  KinematicCollision3D;
+	lCollisionStair0 = move_and_collide(mFullMotion * delta, true);
+	if (lCollisionStair0 and lCollisionStair0.get_collision_count() > 0):
+		self.position = lPositionStart \
+			# Move to contact position
+			+ lCollisionStair0.get_travel() \
+			# Move a bit into the stair
+			+ lCollisionStair0.get_travel().normalized() * min(cStairPenetrationBias, lCollisionStair0.get_remainder().length()) \
+			# Start above the max stair height
+			+ mFloorNormal * (cStairMaxHeight + cFlatMotionBias);
+		# Cast downward
+		var lCollisionStair1 : KinematicCollision3D; 
+		lCollisionStair1 = move_and_collide(-mFloorNormal * cStairMaxHeight, true);
+		# We have a stair!
+		if (lCollisionStair1 and lCollisionStair1.get_collision_count() > 0):
+			# Was there any space to move?
+			if (lCollisionStair1.get_travel().length_squared() > 0):
+				# Pull the starting position upwards
+				var lStairHeight : float = cStairMaxHeight - lCollisionStair1.get_travel().length();
+				print("stair height %f" % [lStairHeight]);
+				lPositionStart += mFloorNormal * max(0, lStairHeight + cFlatMotionBias);
+				# Mark we have ground so we can get our slide onto the stair:
+				lHasGround = true;
+				
+				# Add offset
+				mStablizerOffsetCamera -= mFloorNormal * max(0, lStairHeight + cFlatMotionBias);
+				mStablizerOffsetModel -= mFloorNormal * max(0, lStairHeight + cFlatMotionBias);
+			pass
+		pass
+	# TODO: Move with contact down stairs when on the ground (ground "glue")
+		
+	if not lHasGround:
+		# Recombine the "flat" motion with the 3D motion
+		mFullMotion.y = self.velocity.y;
+		# Do gravity
+		mFullMotion += self.get_gravity() * delta;
+	else:
+		# Stop vertical motion
+		mFullMotion.y = 0;
+	
+	# Reset position
+	self.position = lPositionStart;
+
+	# Update ground check
+	var lCollisionFloor0 : KinematicCollision3D;
+	lCollisionFloor0 = move_and_collide(-mFloorNormal * cFlatMotionBias * 2.0, true);
+	if (lCollisionFloor0 and lCollisionFloor0.get_collision_count() > 0):
+		mOnGround = true;
+	else:
+		mOnGround = false;
+	
+	if mOnGround:
+		mDisableSpeedLimitUntilGround = false;
+		
+	# Do grinding
+	#physicsProcessGrindPaths(delta);
+	
+	# Do jumping logic
+	if mJumpNextPhysicsFrame:
+		mJumpNextPhysicsFrame = false;
+		if mOnGround:
+			mFullMotion.y = cJumpImpulse;
+		pass
+	
+	# Do motion checks now
+	self.velocity = mFullMotion;
+	move_and_slide();
+	
+	# Read back resulting motion
+	mFullMotion = self.velocity;
+	
+	if lHasGround:
+		print("vertical speed %f" % mFullMotion.y);
+	pass
